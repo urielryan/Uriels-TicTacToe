aa3b39f619fcf25c1e617ef6cb332e94d0b60f74
unknown <urielryanreyles@gmail.com>
2022-06-12 00:04:16 +0800

    all buttons can now be disabled after click

--- Real TicTacToe/Form1.cs
+++ Real TicTacToe/Form1.cs
@@ -28,6 +28,11 @@ namespace Real_TicTacToe
           
             
         }
+        
+        
+            
+        
+       
 
         private void upperbttn1CLICK(object sender, EventArgs e)
         {
@@ -36,46 +41,80 @@ namespace Real_TicTacToe
             {
                 upperbttn1.Enabled = false;
             }
+
+           
         }
 
         private void upperbttn2CLICK(object sender, EventArgs e)
         {
             upperbttn2.Text = "X";
+            if (upperbttn2.Enabled = true)
+            {
+                upperbttn2.Enabled = false;
+            }
         }
 
         private void upperbttn3CLICK (object sender, EventArgs e)
         {
             upperbttn3.Text = "X";
+            if (upperbttn3.Enabled = true)
+            {
+                upperbttn3.Enabled = false;
+            }
         }
 
         private void midbttn1CLICK(object sender, EventArgs e)
         {
             midbttn1.Text = "X";
+            if (midbttn1.Enabled = true)
+            {
+                midbttn1.Enabled = false;
+            }
         }
 
         private void midbttn2CLICK(object sender, EventArgs e)
         {
             midbttn2.Text = "X";
+            if (midbttn2.Enabled = true)
+            {
+                midbttn2.Enabled = false;
+            }
         }
 
         private void midbttn3CLICK(object sender, EventArgs e)
         {
             midbttn3.Text = "X";
+            if (midbttn3.Enabled = true)
+            {
+                midbttn3.Enabled = false;
+            }
         }
 
         private void lowerbttn1CLICK(object sender, EventArgs e)
         {
             lowerbttn1.Text = "X";
+            if (lowerbttn1.Enabled = true)
+            {
+                lowerbttn1.Enabled = false;
+            }
         }
 
         private void lowerbttn2CLICK(object sender, EventArgs e)
         {
             lowerbttn2.Text = "X";
+            if (lowerbttn2.Enabled = true)
+            {
+                lowerbttn2.Enabled = false;
+            }
         }
 
         private void lowerbttn3CLICK(object sender, EventArgs e)
         {
             lowerbttn3.Text = "X";
+            if (lowerbttn3.Enabled = true)
+            {
+                lowerbttn3.Enabled = false;
+            }
         }
     }
 
